011f964591781a3796423869e042230a0f0ffbe7
Timur0204 <timur02042008@gmail.com>
2023-12-30 17:53:38 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,44 +10,50 @@
     const hundreds = ['', 'сто', 'двісті', 'триста', 'чотириста', 'пятсот', 'шістсот', 'сімсот', 'вісімсот', 'девятсот'];
 
     let result = '';
-    let hundredsDigit = Math.floor(userNumber / 100);
-    let tensDigit = Math.floor((userNumber % 100) / 10);
-    let unitsDigit = userNumber % 10;
 
-    if (hundredsDigit > 0) {
-        result += hundreds[hundredsDigit] + ' ';
-    }
+    if (!isNaN(userNumber) && userNumber !== '') {
 
-    if (tensDigit >= 2) {
-        result += tensPartTwo[tensDigit] + ' ';
-        if (unitsDigit > 0) {
-            result += unitsAlternativ[unitsDigit];
+        if (userNumber >= 1 && userNumber <= 999) {
+            let hundredsDigit = Math.floor(userNumber / 100);
+            let tensDigit = Math.floor((userNumber % 100) / 10);
+            let unitsDigit = userNumber % 10;
+
+            if (hundredsDigit > 0) {
+                result += hundreds[hundredsDigit] + ' ';
+            }
+
+            if (tensDigit === 1) {
+                result += tens[userNumber % 100 - 10];
+            } else {
+                if (tensDigit >= 2) {
+                    result += tensPartTwo[tensDigit] + ' ';
+                    if (unitsDigit > 0) {
+                        result += unitsAlternativ[unitsDigit];
+                    }
+                } else if (unitsDigit > 0) {
+                    result += units[unitsDigit];
+                }
+            }
+
+            let lastTwoDigits = userNumber % 100;
+            if ((lastTwoDigits >= 11 && lastTwoDigits <= 14) || (lastTwoDigits % 10 === 0)) {
+                result += ' гривень';
+            } else {
+                let prelastDigit = userNumber % 10;
+                if (prelastDigit >= 5 || prelastDigit === 0) {
+                    result += ' гривень';
+                } else if (prelastDigit >= 2 && prelastDigit <= 4) {
+                    result += ' гривні';
+                } else if (userNumber === 1) {
+                    result += ' гривня';
+                } else {
+                    result += ' гривня';
+                }
+            }
         }
-    } else if (tensDigit === 1) {
-        result += tens[userNumber % 100 - 10];
-    } else if (unitsDigit > 0) {
-        result += units[unitsDigit];
-    }
-
-    if (userNumber > 1 && userNumber <= 4) {
-        result += ' гривні';
-    } else if (userNumber >= 5 && userNumber <= 20) {
-        result += ' гривень';
     } else {
-        let prelastDigit = userNumber % 10;
-        if (prelastDigit >= 5 || prelastDigit === 0) {
-            result += ' гривень';
-        } else if (prelastDigit >= 2 && prelastDigit <= 4 ) {
-            result += ' гривні';
-        } else if(userNumber == 1) {
-            result += ' гривня'
-        } else if (isNaN(userNumber) || userNumber == '') {
-            resultNumber.value = 'Введіть коректну суму';
-            return; 
-        } else {
-            result += ' гривня';
-        }
+        resultNumber.value = 'Введіть коректне число';
     }
 
     resultNumber.value = result;
-}
+   }
